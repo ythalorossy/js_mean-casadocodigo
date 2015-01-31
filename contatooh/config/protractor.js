faec87ebb57c795ed03ee59cf0ed86752271d69f
ythalorossy <ythalorossy@gmail.com>
2015-01-31 17:36:48 -0300

    Capitulo 11 finalizado - testes - e2e

---
+++ contatooh/config/protractor.js
@@ -0,0 +1,13 @@
+exports.config = {
+    
+    specs: ['../test/e2e/**/*.js'],
+    
+    onPrepare: function () {
+    
+        browser.driver.get('http://localhost:3000');
+        browser.driver.findElement(by.xpath(".//*[@id='entrar']/h1")).click();
+        browser.driver.findElement(by.id('login_field')).sendKeys('ythalorossy@gmail.com');
+        browser.driver.findElement(by.id('password')).sendKeys('Rabitoto99$');
+        browser.driver.findElement(by.name('commit')).click();
+    }
+};
