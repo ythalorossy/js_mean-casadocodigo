c1438f19e2c91c0d724ae966c63224e215dd9b87
ythalorossy <ythalorossy@gmail.com>
2015-02-12 21:36:31 -0300

    ... xxx ...

--- config/protractor.js
+++ config/protractor.js
@@ -7,7 +7,7 @@ exports.config = {
         browser.driver.get('http://localhost:3000');
         browser.driver.findElement(by.xpath(".//*[@id='entrar']/h1")).click();
         browser.driver.findElement(by.id('login_field')).sendKeys('ythalorossy@gmail.com');
-        browser.driver.findElement(by.id('password')).sendKeys('Rabitoto99$');
+        browser.driver.findElement(by.id('password')).sendKeys('$$$');
         browser.driver.findElement(by.name('commit')).click();
     }
 };
