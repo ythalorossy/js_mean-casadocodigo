ff97f66234bf6648c26861edd57c4a078dda31a5
ythalorossy <ythalorossy@gmail.com>
2015-02-13 15:37:41 -0300

    Configurações do Sauce Labs

--- config/protractor.js
+++ config/protractor.js
@@ -1,22 +1,22 @@
 var config = require('./config')();
 
 exports.config = {
-    sauceUser : config.sauceUser,
-    sauceKey : config.sauceKey,
-    capabilities : {
-        'name' : config.sauceTestName,
-        'browserName' : 'chrome',
-        'tunnel-identifier' : config.travisJubNumber,
-        'build' : config.travisBuild
+    sauceUser: config.sauceUser,
+    sauceKey: config.sauceKey,
+    capabilities: {
+        'name': config.sauceTestName,
+        'browserName': 'chrome',
+        'tunnel-identifier': config.travisJobNumber,
+        'build': config.travisBuild
     },
-    
-    specs: ['../test/e2e/**/*.js'],
-    
+    specs: ['../test/e2e/**/*Spec.js'],
     onPrepare: function () {
         browser.driver.get('http://localhost:3000');
-        browser.driver.findElement(by.xpath(".//*[@id='entrar']/h1")).click();
-        browser.driver.findElement(by.id('login_field')).sendKeys(config.seleniumUser);
-        browser.driver.findElement(by.id('password')).sendKeys(config.seleniumUserPassword);
+        browser.driver.findElement(by.id('entrar')).click();
+        browser.driver.findElement(by.id('login_field'))
+            .sendKeys(config.seleniumUser);
+        browser.driver.findElement(by.id('password'))
+            .sendKeys(config.seleniumUserPassword);
         browser.driver.findElement(by.name('commit')).click();
     }
 };
