a33b80fdc42e124386244b32caa85e0ea3254719
ythalorossy <ythalorossy@gmail.com>
2015-02-13 16:41:16 -0300

    Correção no click na tela Entrar pelo github

--- config/protractor.js
+++ config/protractor.js
@@ -12,7 +12,7 @@ exports.config = {
     specs: ['../test/e2e/**/*Spec.js'],
     onPrepare: function () {
         browser.driver.get('http://localhost:3000');
-        browser.driver.findElement(by.id('entrar')).click();
+        browser.driver.findElement(by.xpath(".//*[@id='entrar']/h1")).click();
         browser.driver.findElement(by.id('login_field')).sendKeys(config.seleniumUser);
         browser.driver.findElement(by.id('password')).sendKeys(config.seleniumUserPassword);
         browser.driver.findElement(by.name('commit')).click();
